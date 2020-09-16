857dbd34a44f8ca62f26e6eb20dac6d7915428ff
t5418042 <makihiro195@ezweb.ne.jp>
2020-09-16 04:17:20 +0000

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -42,12 +42,49 @@ class LinebotController < ApplicationController
                         text: "家を探すor家を建てる https://www.kurashimanet.jp/lifestyle/house/"
                     }                    
                     client.reply_message(event["replyToken"], message)
+                    
                 elsif event.message['text'] == "教育"
                     client.reply_message(event["replyToken"], template2)
-                elsif event.message['text'] == "経済"
+                elsif event.message['text'] == "経済的支援"
                     message = {
                         type: "text",
-                        text: "様々な経済的支援を受けられます！ https://www.pref.shimane.lg.jp/medical/fukushi/hitori/hitori_oya_katei/keizaisien.html"
+                        text: "様々な経済的支援を受けられます！ \n https://www.pref.shimane.lg.jp/medical/fukushi/hitori/hitori_oya_katei/keizaisien.html"
+                    }
+                    client.reply_message(event["replyToken"], message)
+                elsif event.message['text'] == "直接相談"
+                    message = {
+                        type: "text",
+                        text: "種類に応じて窓口が存在します！ \n https://www.pref.shimane.lg.jp/education/kyoiku/iinkai/sodan/"
+                    }
+                    client.reply_message(event["replyToken"], message)    
+                
+                elsif event.message['text'] == "子育て"
+                    client.reply_message(event["replyToken"], template3)
+                elsif event.message['text'] == "支援制度"
+                    message = {
+                        type: "text",
+                        text: "県の情報がまとめられています！ \n https://www.kurashimanet.jp/lifestyle/child-rearing/"
+                    }
+                    client.reply_message(event["replyToken"], message)
+                elsif event.message['text'] == "電話相談"
+                    message = {
+                        type: "text",
+                        text: "島根では専門スタッフが相談に応じます！ \n https://www.pref.shimane.lg.jp/education/child/kodomo/gyakutai/dennwasoudannmadoguti.html"
+                    }
+                    client.reply_message(event["replyToken"], message)
+                    
+                 elsif event.message['text'] == "医療福祉"
+                    client.reply_message(event["replyToken"], template2)
+                elsif event.message['text'] == "福祉医療支援制度"
+                    message = {
+                        type: "text",
+                        text: "島根県では様々な福祉医療費助成制度があります！ \n https://www.pref.shimane.lg.jp/medical/fukushi/syougai/ippan/fukushiaramashi/iryo_3.html"
+                    }
+                    client.reply_message(event["replyToken"], message)
+                elsif event.message['text'] == "乳幼児・子ども医療支援制度"
+                    message = {
+                        type: "text",
+                        text: "島根県では各市町村で乳幼児・子供医療で手厚い費用助成制度があります！ \n http://www.shimane-kokuho.or.jp/gb03_health/other/1-1ika_other2.pdf"
                     }
                     client.reply_message(event["replyToken"], message)
                 end
@@ -97,19 +134,65 @@ class LinebotController < ApplicationController
             "altText": "this is a confirm template",
             "template": {
                 "type": "confirm",
-                "text": "どちら？",
+                "text": "どちらに興味がありますか？",
                 "actions": [
                     {
                       "type": "message",
                       # Botから送られてきたメッセージに表示される文字列
-                      "label": "経済",
+                      "label": "経済的支援",
                       # ボタンを押した時にBotに送られる文字列
-                      "text": "経済"
+                      "text": "経済的支援"
                     },
                     {
                       "type": "message",
-                      "label": "支援",
-                      "text": "支援"
+                      "label": "直接相談",
+                      "text": "直接相談"
+                    }
+                ]
+            }
+          }
+        def template3
+          {
+            "type": "template",
+            "altText": "this is a confirm template",
+            "template": {
+                "type": "confirm",
+                "text": "どちらに興味がありますか？",
+                "actions": [
+                    {
+                      "type": "message",
+                      # Botから送られてきたメッセージに表示される文字列
+                      "label": "支援制度",
+                      # ボタンを押した時にBotに送られる文字列
+                      "text": "支援制度"
+                    },
+                    {
+                      "type": "message",
+                      "label": "電話相談",
+                      "text": "電話相談"
+                    }
+                ]
+            }
+          }
+        def template4
+          {
+            "type": "template",
+            "altText": "this is a confirm template",
+            "template": {
+                "type": "confirm",
+                "text": "どちらに興味がありますか？",
+                "actions": [
+                    {
+                      "type": "message",
+                      # Botから送られてきたメッセージに表示される文字列
+                      "label": "福祉医療支援制度",
+                      # ボタンを押した時にBotに送られる文字列
+                      "text": "福祉医療支援制度"
+                    },
+                    {
+                      "type": "message",
+                      "label": "乳幼児・子ども医療支援制度",
+                      "text": "乳幼児・子ども医療支援制度"
                     }
                 ]
             }
